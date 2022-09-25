9fae29c2eb95f61576176f5d7213c3b3d20f77cf
Amit kumar <ak0638010@gmail.com>
2022-09-26 00:29:57 +0530

    26! about and menu

--- src/pages/CafeDetail.js
+++ src/pages/CafeDetail.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "../components/Slider/Slider";
+import luffy from "../img/luffy.jpg";
 import {
   Container,
   Wrapper,
@@ -7,19 +8,72 @@ import {
   Description,
   NavContainer,
   NavElements,
+  About,
+  AboutCafe,
+  ElementContainer,
+  Title,
+  Element,
+  Menu,
+  MenuImage,
 } from "../styledComponents/CafeDetail";
 
 const CafeDetail = () => {
+
+  const [open, setOpen] = useState(false);
+
   return (
     <Container>
       <Slider></Slider>
       <Wrapper>
         <Heading>Chai Sutta</Heading>
         <Description>
-         <NavContainer>
-         <NavElements>About</NavElements>
-         <NavElements>Menu</NavElements>
-         </NavContainer>
+          <NavContainer>
+            <NavElements onClick={() => setOpen(true)} >About</NavElements>
+            <NavElements  onClick={() => setOpen(false)} >Menu</NavElements>
+          </NavContainer>
+          {open ? (
+            <>
+              <About>
+                <ElementContainer>
+                  <Title>Servie:</Title>
+                  <Element>Dine-in, Takeaway</Element>{" "}
+                </ElementContainer>
+                <ElementContainer>
+                  <Title>Address:</Title>
+                  <Element>Dine-in, Takeaway</Element>{" "}
+                </ElementContainer>
+                <ElementContainer>
+                  <Title>Servie:</Title>
+                  <Element>Dine-in, Takeaway</Element>{" "}
+                </ElementContainer>
+                <ElementContainer>
+                  <Title>Servie:</Title>
+                  <Element>Dine-in, Takeaway</Element>{" "}
+                </ElementContainer>
+                <ElementContainer>
+                  <Title>Servie:</Title>
+                  <Element>Dine-in, Takeaway</Element>{" "}
+                </ElementContainer>
+
+                <AboutCafe>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Animi suscipit labore eveniet, pariatur totam, cumque
+                    obcaecati a facilis quisquam facere molestias nesciunt,
+                    earum eaque officia! Perspiciatis atque repellat a, officia
+                    delectus velit sit rerum nihil eius minus hic officiis
+                    debitis, natus provident impedit et commodi nostrum
+                    repudiandae autem accusamus dolore.
+                  </p>
+                </AboutCafe>
+              </About>
+            </>
+          ) : null}
+          
+            {
+                !open ? <Menu> <MenuImage src = {luffy} /> </Menu>: null
+
+            }
         </Description>
       </Wrapper>
     </Container>
--- src/styledComponents/CafeDetail.js
+++ src/styledComponents/CafeDetail.js
@@ -32,8 +32,10 @@ align-items: center;
 
 export const Description = styled.div`
 width: 100%;
+max-width: 90%;
 // height: 20px;
 border: 1px solid white;
+padding-left: 1%
 
 `
 export const NavContainer = styled.div`
@@ -58,6 +60,76 @@ height: 30px;
 display: felx;
 justify-content: center;
 align-items: center;
+color: white;
+cursor: pointer;
+
+`
+
+
+
+export const About = styled.div`
+width: 100%;
+// display: flex;
+// align-item: center;
+// justify-content: center;
+color: white;
+// border: 1px solid red;
+
+`
+
+export const ElementContainer = styled.div`
+display: flex;
+gap: 10px;
+align-items: center;
+
+
+
+
+`
+
+export const Title = styled.h4`
+font-size: 17px;
+color: #7d9aaf;
+font-family: Arial, Helvetica, sans-serif;
+
+
+`
+
+export const Element = styled.h5`
+color: #beb695;
+font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif
+
+`
+
+
+export const AboutCafe = styled.div`
+width: 100%;
+border-top: 1px solid grey;
+font-size: 14px;
+margin-top: 10px;
+padding-top: 10px; 
+font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+color: #8f8e8b;
+
+`
+
+export const Menu = styled.div`
+width: 100%;
+max-width: 100%;
+display: flex;
+justify-content: center;
+object-fit: cover;
+padding-bottom: 30%;
+
+
+`
+
+export const MenuImage = styled.img`
+// height: 35%;
+width: 50%;
+@media only screen and (max-width: 500px) {
+    width: 95%;
+}
 
 `
 
